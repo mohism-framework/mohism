3a18deed8394869e904450b74697c2fc46a9baff
蓝 <lanhao@class100.com>
2019-10-18 23:50:43 +0800

    feat: self upgrade!

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -1,17 +1,26 @@
 #!/usr/bin/env node
+import Command from '@mohism/cli-wrapper/dist/libs/command.class';
+import Upgrade from '@mohism/self-upgrade';
+import { resolve } from 'path';
+
+import Create from '../commands/create.action';
+import Init from '../commands/init.action';
+
 require('colors');
 
-import { resolve } from 'path'; 
-import Command from '@mohism/cli-wrapper/dist/libs/command.class';
-
+const pkg = require('../../package.json');
 // init
 const instance = new Command({
+  name: Object.keys(pkg.bin)[0],
   root: resolve(`${__dirname}/../..`),
   home: process.env.HOME,
   version: '1.0.0',
 });
 
 // register
+instance.add('init', Init);
+instance.add('create', Create);
+instance.add('upgrade', Upgrade);
 
 // run
 instance.run();
